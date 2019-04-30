170fc2dae8ee171b0ab124e611c7444954a50d37
Karl Shifflett <kdawg1406@gmail.com>
2019-04-30 19:13:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Tame Visual Studio Editor Tool Tips
+
+<!-- Update the VS Gallery link after you upload the VSIX-->
+Download this extension from the [VS Gallery](https://marketplace.visualstudio.com/vsgallery/097521bb-c432-4436-ba9f-137dcb03d3be)
+
+---------------------------------------
+
+## Feature
+Getting tired of seeing Visual Studio code editor tooltips when you're presenting?  They flash immediately when you're moving your mouse.  Very distracting for you and your viewers.
+
+This extension "Tames" the Visual Studio 2017 & 2019 code editor Tooltips by not showing them unless the LEFT CTRL key or LEFT SHIFT is depressed and the user hovers over the object.  
+
+### Building
+The project was built using Visual Studio 2019.
+
+For cloning and building this project yourself, make sure to install 
+[Extensibility Tools 2019](https://marketplace.visualstudio.com/items?itemName=MadsKristensen.ExtensibilityEssentials2019)
+extension for Visual Studio which enables some features
+used by this project.
+
+## License
+[Apache 2.0](LICENSE)
---
+++ TameVisualStudioEditorToolTips/TameQuickInfo.cs
@@ -0,0 +1,48 @@
+﻿namespace TameVisualStudioEditorToolTips {
+
+    using System.ComponentModel.Composition;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Microsoft.VisualStudio.Language.Intellisense;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(IAsyncQuickInfoSourceProvider))]
+    [Name("Tame Quick Info")]
+    [Order(Before = "Default Quick Info Presenter")]
+    [ContentType("CSharp")]
+    [ContentType("XAML")]
+    [ContentType("XML")]
+    internal class TameQuickInfo : IAsyncQuickInfoSourceProvider {
+
+        public IAsyncQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer) {
+            return textBuffer.Properties.GetOrCreateSingletonProperty(() => new CancelingQuickInfoSource());
+        }
+    }
+
+    internal class CancelingQuickInfoSource : IAsyncQuickInfoSource {
+
+        public CancelingQuickInfoSource() {
+        }
+
+        public void Dispose() {
+        }
+
+        public async Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken) {
+            var isKeyDown = false;
+            ThreadHelper.JoinableTaskFactory.Run(async delegate {
+                // now on the UI thread
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                isKeyDown = Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.LeftShift);
+            });
+
+            // back on background thread
+            if (!isKeyDown) {
+                _ = session.DismissAsync();
+            }
+            return await System.Threading.Tasks.Task.FromResult<QuickInfoItem>(null);
+        }
+    }
+}
